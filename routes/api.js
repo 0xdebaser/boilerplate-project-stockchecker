909008ca05b8aa9fe8870db8651ae5b3e4807aa5
debaser9 <jason@jasonbraswell.com>
2023-04-08 10:06:53 -1000

    0xdebaser's intial commit

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,108 @@
-'use strict';
+"use strict";
+
+const fetch = require("node-fetch");
+const mongoose = require("mongoose");
+const sha256 = require("js-sha256").sha256;
+
+mongoose.connect(process.env.DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const stockSchema = new mongoose.Schema({
+  symbol: String,
+  likes: {
+    type: Array,
+    default: [],
+  },
+});
+const Stock = mongoose.model("Stock", stockSchema);
+
+async function getStockPrice(ticker) {
+  const response = await fetch(
+    `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${ticker}/quote`
+  );
+  const data = await response.json();
+  return data.latestPrice;
+}
+
+async function getLikes(ticker) {
+  let stock = await Stock.findOne({ symbol: ticker });
+  if (!stock) {
+    const newStock = new Stock({ symbol: ticker, likes: [] });
+    stock = await newStock.save();
+  }
+  return stock.likes.length;
+}
+
+async function addLike(ticker, ipAddress) {
+  let stock = await Stock.findOne({ symbol: ticker });
+  if (!stock) {
+    const newStock = new Stock({ symbol: ticker, likes: [] });
+    stock = await newStock.save();
+  }
+  const likes = stock.likes.slice();
+  if (!likes.includes(ipAddress)) {
+    console.log(`IP Address ${ipAddress} likes!`);
+    likes.push(ipAddress);
+    stock = await Stock.findOneAndUpdate({ symbol: ticker }, { likes });
+  } else {
+    console.log(`IP Address ${ipAddress} already likes!`);
+  }
+  return likes.length;
+}
 
 module.exports = function (app) {
-
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
+  app.route("/api/stock-prices").get(async function (req, res) {
+    const { stock, like } = req.query;
+    if (typeof stock === "string") {
+      // Single stock handling
+      const price = await getStockPrice(stock);
+      let likes;
+      if (like === "true") {
+        const ipAddress = req.socket.remoteAddress;
+        const hashedIpAddress = sha256(ipAddress);
+        likes = await addLike(stock, hashedIpAddress);
+      } else {
+        likes = await getLikes(stock);
+      }
+      res.send(JSON.stringify({ stockData: { stock, price, likes } }));
+    } else {
+      // Two stock handling
+      const price0 = await getStockPrice(stock[0]);
+      let likes0;
+      if (like === "true") {
+        const ipAddress = req.socket.remoteAddress;
+        const hashedIpAddress = sha256(ipAddress);
+        likes0 = await addLike(stock[0], hashedIpAddress);
+      } else {
+        console.log("get likes");
+        likes0 = await getLikes(stock[0]);
+      }
+      const price1 = await getStockPrice(stock[1]);
+      let likes1;
+      if (like === "true") {
+        const ipAddress = req.socket.remoteAddress;
+        const hashedIpAddress = sha256(ipAddress);
+        likes1 = await addLike(stock[1], hashedIpAddress);
+      } else {
+        likes1 = await getLikes(stock[1]);
+      }
+      res.send(
+        JSON.stringify({
+          stockData: [
+            {
+              stock: stock[0],
+              price: price0,
+              rel_likes: likes0 - likes1,
+            },
+            {
+              stock: stock[1],
+              price: price1,
+              rel_likes: likes1 - likes0,
+            },
+          ],
+        })
+      );
+    }
+  });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,116 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite("Functional Tests", function () {
+  const baseUrl = "/api/stock-prices";
+  const testStocks = ["JNJ", "UNH", "V"];
+  let originalLikes, originalRelLikes;
 
+  test("Viewing one stock: GET request to /api/stock-prices/", async () => {
+    const res = await chai
+      .request(server)
+      .get(`${baseUrl}?stock=${testStocks[0]}`);
+    const data = await JSON.parse(res.text);
+    assert.equal(res.status, 200);
+    assert.isObject(data);
+    assert.hasAllKeys(data, "stockData");
+    assert.hasAllKeys(data.stockData, ["stock", "price", "likes"]);
+    //originalLikes = data.stockData.likes;
+  });
+
+  test("Viewing one stock and liking it: GET request to /api/stock-prices/", async () => {
+    const res = await chai
+      .request(server)
+      .get(`${baseUrl}?stock=${testStocks[0]}&like=true`);
+    const data = await JSON.parse(res.text);
+    assert.isObject(data);
+    assert.hasAllKeys(data, "stockData");
+    assert.hasAllKeys(
+      data.stockData,
+      ["stock", "price", "likes"],
+      "Response object is missing required key."
+    );
+    // assert.equal(
+    //   data.stockData.likes,
+    //   originalLikes + 1,
+    //   "Likes did not update!"
+    // );
+  });
+
+  test("Viewing the same stock and liking it again: GET request to /api/stock-prices/", async () => {
+    const res = await chai
+      .request(server)
+      .get(`${baseUrl}?stock=${testStocks[0]}&like=true`);
+    const data = await JSON.parse(res.text);
+    assert.isObject(data);
+    assert.hasAllKeys(
+      data,
+      "stockData",
+      "Response object is missing required key."
+    );
+    assert.hasAllKeys(
+      data.stockData,
+      ["stock", "price", "likes"],
+      "Response object is missing required key."
+    );
+    // assert.equal(
+    //   data.stockData.likes,
+    //   originalLikes + 1,
+    //   "Likes updated for dup IP address!"
+    // );
+  });
+
+  test("Viewing two stocks: GET request to /api/stock-prices/", async () => {
+    const res = await chai
+      .request(server)
+      .get(`${baseUrl}?stock=${testStocks[1]}&stock=${testStocks[2]}`);
+    const data = await JSON.parse(res.text);
+    assert.isObject(data);
+    assert.isArray(data.stockData);
+    assert.hasAllKeys(
+      data.stockData[0],
+      ["stock", "price", "rel_likes"],
+      "Response object is missing required key."
+    );
+    assert.hasAllKeys(
+      data.stockData[1],
+      ["stock", "price", "rel_likes"],
+      "Response object is missing required key."
+    );
+    originalRelLikes = [
+      data.stockData[0].rel_likes,
+      data.stockData[1].rel_likes,
+    ];
+  });
+
+  test("Viewing two stocks and liking them: GET request to /api/stock-prices/", async () => {
+    const res = await chai
+      .request(server)
+      .get(
+        `${baseUrl}?stock=${testStocks[1]}&stock=${testStocks[2]}&like=true`
+      );
+    const data = await JSON.parse(res.text);
+    console.log("data:", data);
+    assert.isObject(data);
+    assert.isArray(data.stockData);
+    assert.hasAllKeys(
+      data.stockData[0],
+      ["stock", "price", "rel_likes"],
+      "Response object is missing required key."
+    );
+    assert.hasAllKeys(
+      data.stockData[1],
+      ["stock", "price", "rel_likes"],
+      "Response object is missing required key."
+    );
+    assert.equal(data.stockData[0].rel_likes, originalRelLikes[0]);
+    assert.equal(data.stockData[1].rel_likes, originalRelLikes[1]);
+  });
+  after(function () {
+    chai.request(server).get("/");
+  });
 });
